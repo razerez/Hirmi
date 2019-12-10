0fb0ad36ef1a8a0e04713463f9240c3f59c0bd76
BenLevitin <benhagadol@gmail.com>
2019-12-10 19:17:34 +0200

    record twice at the same time and save it

--- Hirmi/Hirmi.py
+++ Hirmi/Hirmi.py
@@ -2,10 +2,21 @@ import sounddevice as sd
 from scipy.io.wavfile import write
 from pydub import AudioSegment
 from scipy.io import wavfile
+import threading
 
-fs = 44100  # Sample rate
-seconds = 2 # Duration of recording
 
-myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
-sd.wait()  # Wait until recording is finished
-write('output.wav', fs, myrecording)  # Save as WAV file
+def record_audio(path):
+    fs = 44100  # Sample rate
+    seconds = 2# Duration of recording
+
+    myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+    sd.wait()  # Wait until recording is finished
+    write(path, fs, myrecording)  # Save as WAV file
+
+
+def main():
+    for index in range(1,3):
+        x = threading.Thread(target=record_audio, args=(str(index)+".wav",))
+        x.start()
+
+main()
