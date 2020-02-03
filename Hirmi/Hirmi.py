db90e61f5fa86a412561a4b78004543fa3145177
raz erez <razerez12@gmail.com>
2020-02-03 18:32:23 +0200

    extreme messure

--- Hirmi/Hirmi.py
+++ Hirmi/Hirmi.py
@@ -13,38 +13,53 @@ import threading
 wav1 = "wav1.wav"
 # CAE - Cut at end:
 wav2 = "wav2.wav"
+amount = 1
 
 
 def main():
     # transfer record mics and transfer audio to wav:
-    t1 = threading.Thread(target=audio_to_wav, args=(wav1, 2,))
-    t2 = threading.Thread(target=audio_to_wav, args=(wav2, 1,))
+    t1 = threading.Thread(target=audio_to_wav, args=(wav1, 3, 10,))
+    t2 = threading.Thread(target=audio_to_wav, args=(wav2, 2, 10,))
     t1.start()
     t2.start()
     t1.join()
     t2.join()
     # play the wav files audio
-    #playsound(wav1)
-    #playsound(wav2)
-    draw_wav(wav1, False)
-    draw_wav(wav1, True)
-    #draw_wav(wav2, False)
-    #draw_wav(wav2, True)
-
+    # playsound(wav1)
+    # playsound(wav2)
+    arr1, smp1 = fix_arr(wav1, True)
+    draw_wav(arr1, smp1, "mic x6")
+    arr2, smp2 = fix_arr(wav2, True)
+    draw_wav(arr2, smp2, "mic x7")
+    compare_extreme_points(arr1, smp1, arr2, smp2)
     # print delay between sound files:
 
 
-def audio_to_wav(dst, device):
+def compare_extreme_points(arr1, smp1, arr2, smp2):
+    exp1 = get_extreme_time(arr1, smp1)
+    exp2 = get_extreme_time(arr2, smp2)
+    time_gap = abs(exp1 - exp2)
+    print(time_gap)
+
+
+def get_extreme_time(arr, smp):
+    time_arr = get_time_arr(arr, smp)
+    min_index = numpy.where(arr == min(arr))[0][0]
+    return time_arr[min_index]
+
+
+def audio_to_wav(dst, device, duration):
     """
     converts live audio file to wav file and return sound array
+    :param duration: duration of recording
     :param device: which device to use
     :param dst: destination wav file
     """
-    DELAY = 1
+    _delay_ = 0
     # Sample rate:
     fs = 44100
     # Duration of recording:
-    seconds = DELAY + 1
+    seconds = _delay_ + duration
     # record:
     recording = sd.rec(int(seconds * fs), samplerate=fs, channels=2, device=device)
     print("recording: " + str(device))
@@ -52,20 +67,38 @@ def audio_to_wav(dst, device):
     sd.wait()
     # Save as WAV file:
     write(dst, fs, recording)
-    newAudio = AudioSegment.from_wav(dst)
-    newAudio = newAudio[DELAY*1000:]
-    newAudio.export(dst, format="wav")
+    new_audio = AudioSegment.from_wav(dst)
+    new_audio = new_audio[_delay_*1000:]
+    new_audio.export(dst, format="wav")
 
 
-def draw_wav(file, do):
+def fix_arr(file, avg):
+    samplerate, sound = wavfile.read(file)
+    arr = numpy.array(separate_array(sound), dtype=numpy.float64)
+    if avg:
+        avg_arr(arr)
+    return arr, samplerate
+
+
+def draw_wav(arr, samplerate, name="enter name"):
     """
     draw a graph of the audio in comparison to the time
-    :param file: path to wav file
-    :param do: var for testing avg_arr()
+    :param name: name to write on the graph
+    :param samplerate: sample rate of the sound file
+    :param arr: array to draw
     """
+    time_arr = get_time_arr(arr, samplerate)
+    plt.plot(time_arr, arr)
+    plt.title(name)
+    plt.ylabel("Sound")
+    plt.xlabel("Time")
+
+    plt.show()
+    return arr
+
+
+def get_time_arr(arr, samplerate):
     time_arr = []
-    samplerate, sound = wavfile.read(file)
-    arr = separate_array(sound)
     size = len(arr)
     time = size / samplerate
     frame_time = time / size
@@ -73,14 +106,29 @@ def draw_wav(file, do):
     while i != size:
         time_arr.append(i * frame_time)
         i += 1
-    if do:
-        avg_arr(arr)
-    plt.plot(time_arr, arr)
-    plt.title(file + " " + str(do))
-    plt.ylabel("Sound")
-    plt.xlabel("Time")
+    return time_arr
 
-    plt.show()
+
+def subtract_arrays(big_arr, small_arr):
+    """
+    move SA(small_array) along BA(big_array) and subtract the values from each other, smallest difference means
+    that the arrays are closer
+    :param big_arr: array to move along
+    :param small_arr: array to move on big array
+    :return:
+    """
+    _pow2_ = 2
+    _take_ = 10
+    sub_sum = 0
+    sub_sum_arr = numpy.array([], dtype=numpy.float64)
+    small_arr_slice = small_arr[:_take_]
+    for i in range(0, len(big_arr) - 10, _take_):
+        for j in range(0, _take_):
+            sub_sum += pow(big_arr[i + j] - small_arr_slice[j], _pow2_)
+        sub_sum_arr = numpy.append(sub_sum_arr, sub_sum)
+        sub_sum = 0
+
+    return numpy.where(sub_sum_arr == min(sub_sum_arr))[0][0]
 
 
 def separate_array(arr):
@@ -102,7 +150,6 @@ def avg_arr(arr):
     """
     #  temp arr:
     t_arr = arr[:]
-    amount = 1000
     i = amount - 1
     while i != len(t_arr):
         temp = 0
